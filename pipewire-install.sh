6852f226bbf9c8d3fd92ee4b1bcd336ac0642c2c
sever0815 <sergeilemonow@protonmail.com>
2026-06-28 15:54:54 +0500

    edit

--- pipewire-install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install pipewire pipewire-pulse pipewire-audio-client-libraries wireplumber pulseaudio-utils
-
-systemctl --user stop pulseaudio
-systemctl --user --now enable pipewire pipewire-pulse
-systemctl --user status pipewire pipewire-pulse
